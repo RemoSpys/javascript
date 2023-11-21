53fc9e667627a4ac99e56c59436b611d28fb1b48
RemoSpys <104862738+RemoSpys@users.noreply.github.com>
2023-11-21 11:23:40 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -191,3 +191,36 @@ for (const student of arrayOfStudents) {
     const editedStudentName = student.toUpperCase();
     console.log("For...of loop: ", editedStudentName);
 }
+
+//functions
+function  square(number = 1) {
+    return number+number;
+}
+console.log(square(3));
+
+
+function areaCalc(side1 = 1, side2 = 1) {
+    return side1 * side2
+}
+console.log(areaCalc(3,4));
+
+//arrowFunction
+const squareArrowfunction = (number = 1) => {
+    return number+number;
+}
+console.log(squareArrowfunction(3));
+
+const areaCalcArrowFunction = (side1 = 1, side2 = 1) => {
+    return side1 * side2
+}
+console.log(areaCalcArrowFunction(3,4));
+
+//automaticFunction
+(function thisIsAutomatic() {
+    console.log("Automatic Regular Function")
+})();
+
+//automaticFunctionArrow
+(()=> (
+    console.log("Automatic Arrow Function")
+))();
